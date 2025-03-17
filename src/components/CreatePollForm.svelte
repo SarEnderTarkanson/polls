aa705707d4996ee99ee4c3d116c58af08f7d0ece
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-17 23:21:18 +0100

    created the PollList component / added an event dispatcher to CreatePollForm component / added an initial array of values in the poll variable & handleAdd function to update the polls array

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,5 +1,8 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import Button from "../shared/Button.svelte";
+
+  let dispatch = createEventDispatcher();
   let fields = { question: "", answerA: "", answerB: "" };
   let errors = { question: "", answerA: "", answerB: "" };
   let valid = false;
@@ -26,7 +29,8 @@
       errors.answerB = "";
     }
     if (valid) {
-      console.log("valid", fields);
+      let poll = { ...fields, votesA: 0, votesB: 0, id: Math.random() };
+      dispatch("add", poll);
     }
   };
 </script>
