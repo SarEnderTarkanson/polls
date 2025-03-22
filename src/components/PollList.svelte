380e392b248449d95e5ca118293ac3b006f92cfa
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-22 23:43:49 +0100

    added PollStore for centralized state management (not completed the entire structure yet)

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,12 +1,17 @@
 <script>
+  import PollStore from "../stores/PollStore";
   import PollDetails from "./PollDetails.svelte";
   export let polls = [];
+
+  PollStore.subscribe((data) => {
+    polls = data;
+  });
 </script>
 
 <div class="poll-list">
   {#each polls as poll (poll.id)}
     <div>
-      <PollDetails {poll} on:vote/>
+      <PollDetails {poll} on:vote />
     </div>
   {/each}
 </div>
@@ -15,6 +20,6 @@
   .poll-list {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    -ms-grid-row-align:end;
+    -ms-grid-row-align: end;
   }
 </style>
